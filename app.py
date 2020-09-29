2fe1b57758078ce53914cb8627b4179d9fd67776
Simon Carr <simonjcarr@gmail.com>
2020-09-29 20:19:43 +0100

    Added cors, endpoints and dotenv

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from typing import Optional
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import pyodbc
+from dotenv import load_dotenv
+import db
 
 app = FastAPI()
+
+origins = [
+  "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+  )
+
+#Make values in .env file available
+load_dotenv()
+cursor = db.get_cursor(db.get_db_connection())
 
 class ToDo(BaseModel):
   task: str
@@ -12,4 +29,37 @@ class ToDo(BaseModel):
 
 @app.get("/")
 def get_user_todos():
-  return {"Hello": "world"}
+  tasks = db.select_query("select * from tasks")
+  print(tasks)
+  return tasks
+
+@app.post("/")
+def create_todo(todo: ToDo):
+  sql = f'''
+  insert into tasks (task, complete, username)
+  values('{todo.task}', '{todo.complete}', '{todo.username}')
+  '''
+  result = db.execute_sql(sql)
+  print("******")
+  print(result)
+  print("******")
+  if (result == 0):
+    return "OK"
+  else:
+    return result
+
+@app.delete("/{task_id}")
+def delete_item(task_id: int):
+  result = db.execute_sql(f'''delete from tasks where id = {task_id}''')
+  if (result == 0):
+    return "OK"
+  else:
+    return result
+
+@app.put("/{task_id}")
+def toggle_complete(task_id: int):
+  result = db.execute_sql(f'''update tasks set complete = 1 - complete where id = {task_id}''')
+  if (result == 0):
+    return "OK"
+  else:
+    return result
---
+++ db.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import pyodbc
+import os
+load_dotenv()
+
+def get_db_connection():
+  return pyodbc.connect(f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER=tcp:{os.getenv('DB_HOST')},{os.getenv('DB_PORT')};DATABASE={os.getenv('DB_DATABASE')};UID={os.getenv('DB_USERNAME')};PWD={os.getenv('DB_PASSWORD')}")
+
+def get_cursor(cnxn):
+  print("*******Connecting to Database*********")
+  cursor = cnxn.cursor()
+  cursor.execute("select @@version;")
+  row = cursor.fetchone()
+  while row:
+    print(row[0])
+    row = cursor.fetchone()
+  return cursor
+
+def select_query(query):
+  cursor = get_cursor(get_db_connection())
+  cursor.execute(f'''
+    select * from tasks;
+  ''' )
+  columns = [column[0] for column in cursor.description]
+  results = []
+  for row in cursor.fetchall():
+    results.append(dict(zip(columns, row)))
+  return results
+
+def execute_sql(sql):
+  db = get_db_connection()
+  cursor = get_cursor(db)
+  try:
+    cursor.execute(sql)
+    db.commit()
+    return 0
+  except Exception as e:
+    print(e)
+    return e
