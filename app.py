733ffd4cb3957b6a3de19efc92631d2657842575
Simon Carr <simonjcarr@gmail.com>
2020-09-29 12:12:29 +0100

    Added a ToDo class

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class ToDo(BaseModel):
+  task: str
+  username: str
+  complete: bool
 
 @app.get("/")
 def get_user_todos():
